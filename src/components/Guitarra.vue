b64555a3c1a640fb39253c7d11343dd997c0ee23
Daniel Vergaray <danielvergarayescobar@gmail.com>
2024-04-07 15:43:30 -0500

    avance

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,53 @@
+<script setup>
+import {ref} from 'vue'
+
+const numero = ref (0)
+
+const props = defineProps ({
+  guitarra:{
+    type: Object,
+    required:true
+  }
+})
+
+const incrementar =()=>{
+  numero.value++
+}
+</script>
+
+<template>
+   
+   <div  class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+          <img
+            class="img-fluid"
+            :src="'/img/' + guitarra.imagen + '.jpg'"
+            :alt="'imagen guitarra' + guitarra.nombre"
+          />
+        </div>
+        <div class="col-8">
+          <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+          <p>
+            <p>{{ numero }}</p> <!-- Cuando se esta en template no hacer falta usar el .value (numero.value) a pesar de ser un ref -->
+            {{ guitarra.descripciom }}
+          </p>
+          <p class="fw-black text-primary fs-3">{{ guitarra.precio }}</p>
+          <button 
+          type="button" 
+          class="btn btn-dark w-100"
+          v-on:click="/* numero++ */ incrementar"
+          
+          >
+            Agregar al Carrito
+          </button>
+        </div>
+      </div>
+      <!-- FIN GUITARRA -->
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
